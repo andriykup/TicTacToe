1adc278fbccc22a338383b298258ba14ea39bc54
Andriy Kupinskyy <andriy.khk@gmail.com>
2023-07-04 12:05:23 +0200

    add function that allows player to add mark to a specific spot

--- script.js
+++ script.js
@@ -11,29 +11,49 @@ const Player = (userName) => {
 
 const gameBoard = (() => {
     const boardElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const createBoard = () => {
+    const setBoard = () => {
         let i = 0;
         while(boardElements[i]){
             const boardElement = document.createElement('div');
+            const boardElemenValue = document.createElement('p');
             boardElement.dataset.index = boardElements[i];
-            boardElement.addEventListener('click', boardElementClicked.bind(this, boardElement.dataset.index));
+            if(boardElements[i] != 'number'){
+                boardElemenValue.textContent = boardElements[i];
+            }
+            boardElement.appendChild(boardElemenValue);
+            boardElement.addEventListener('click', boardElementClicked.bind(this, boardElement.dataset.index - 1));
             board.appendChild(boardElement);
             i++;
         }
     }
+
+    function updateBoard(index, value){
+        boardElements[index] = value;
+    }
+
+    function resetBoard(){
+        while(board.firstChild){
+            board.firstChild.remove();
+        }
+    }
+    
+    function boardElementClicked(elementIndex) {
+        console.log("element was clicked");
+        resetBoard();
+        updateBoard(elementIndex, "x");
+        setBoard();
+    }
+    
     return {
-        createBoard,
+        updateBoard,
+        setBoard,
     }
 })();
-
-
-function boardElementClicked(elementIndex) {
-    console.log("boardElement with index: " + elementIndex + " was clicked");
-}
 
 function startgame(){
     //PLayers initialization
     player1 = Player(prompt("Player N1, please introduce you name: "));
     player2 = Player(prompt("Player N2, please introduce you name: "));
-    gameBoard.createBoard();
+    gameBoard.setBoard();
+
 }
