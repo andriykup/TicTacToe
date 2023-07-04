6c7655204f6261df99255b2ec480b8f6cada33dc
Andriy Kupinskyy <andriy.khk@gmail.com>
2023-07-04 02:09:10 +0200

    create gameBoard and Player module/factory

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const gameBoard = document.getElementById('gameBoard');
+const board = document.getElementById('gameBoard');
+const startButton = document.getElementById('startGame');
+startButton.addEventListener('click', startgame);
 
-for(let i = 1; i <= 9; i++){
-    const boardElement = document.createElement('div');
-    boardElement.dataset.index = i;
-    boardElement.addEventListener('click', boardElementClicked.bind(this, boardElement.dataset.index));
-    gameBoard.appendChild(boardElement);
+let player1; //palyer declaration
+let player2; //player declaration
+
+const Player = (userName) => {
+    return {userName};
 }
+
+const gameBoard = (() => {
+    const boardElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const createBoard = () => {
+        let i = 0;
+        while(boardElements[i]){
+            const boardElement = document.createElement('div');
+            boardElement.dataset.index = boardElements[i];
+            boardElement.addEventListener('click', boardElementClicked.bind(this, boardElement.dataset.index));
+            board.appendChild(boardElement);
+            i++;
+        }
+    }
+    return {
+        createBoard,
+    }
+})();
+
 
 function boardElementClicked(elementIndex) {
     console.log("boardElement with index: " + elementIndex + " was clicked");
+}
+
+function startgame(){
+    //PLayers initialization
+    player1 = Player(prompt("Player N1, please introduce you name: "));
+    player2 = Player(prompt("Player N2, please introduce you name: "));
+    gameBoard.createBoard();
 }
